18161eca20a5a49c63207f9b821d0b65ceeb98f0
Guilherme Albano <guilhermenovolg@gmail.com>
2023-02-27 14:39:21 -0300

    Logicos

---
+++ DioBootCamp2/src/opLogicos/AuxilioEmerg.java
@@ -0,0 +1,16 @@
+package opLogicos;
+
+public class AuxilioEmerg {
+	public static void main (String []args) {
+		double salarioMinimo = 1000;
+		double salarioMedioDaFamilia = 1500;
+		
+		int dependentesDaMedia = 4;
+		int mediaDependentes= 2;
+		
+		
+		System.out.println((salarioMinimo < salarioMedioDaFamilia)&& (dependentesDaMedia > mediaDependentes )) ;
+		
+
+	}
+}
